408f4c0cf950327a28562ae4a55ee96e52b0aaa2
kerfmanner <konovluka@gmail.com>
2025-10-06 16:10:03 +0300

    Add tests, corrected xml, default constructor for Flower and getPrice method for FlowerBucket

--- src/main/java/ucu/edu/apps/FlowerBucket.java
+++ src/main/java/ucu/edu/apps/FlowerBucket.java
@@ -5,12 +5,20 @@ import java.util.List;
 
 public class FlowerBucket {
 
-    List<FlowerPack> flowerPacks;
+    private List<FlowerPack> flowerPacks;
 
-    public void addFlowerPack(FlowerPack flowerPack) {
+    public void add(FlowerPack flowerPack) {
         if (flowerPacks == null) {
             flowerPacks = new ArrayList<FlowerPack>();
         }
         flowerPacks.add(flowerPack);
+    }
+    
+    public double getPrice(){
+        double price = 0;
+        for (FlowerPack i : flowerPacks) {
+            price += i.getPrice();
+        }
+        return price;
     }
 }
--- src/main/java/ucu/edu/apps/Main.java
+++ src/main/java/ucu/edu/apps/Main.java
@@ -3,6 +3,5 @@ package ucu.edu.apps;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
     }
 }
